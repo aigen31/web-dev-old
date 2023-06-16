a52c072fafa8543772a1cf44044f363922ed595f
AiGen31 <aigen.chernogorsk@yandex.ru>
2023-06-16 23:45:51 +0700

    Layout Main Page

--- assets/js/common.js
+++ assets/js/common.js
@@ -9,24 +9,51 @@ $(document).ready(function() {
     nextArrow: '<button class="s-main__slider-btn"><img src="assets/img/arrows/slider-down.svg" alt=""/></button>'
   });
 
+
+  let resize = () => {
+    if ($(window).width() >= 1400) {
+      $('.s-main').css('padding-right', $('.right-line').width())
+      $('.s-main').css('padding-left', $('.right-line').width())
+    } else {
+      $('.s-main').css('padding-right', '')
+      $('.s-main').css('padding-left', '')
+    }
+  }
+
+  $(window).resize(resize)
+
+  $('#hamburger__main').click(function() {
+    $('.menu-wrapper').toggleClass('active');
+  })
+
+  $('#hamburger__main').hover(function() {
+    $(this).addClass('is-active');
+  }, function () {
+    $(this).removeClass('is-active');
+  },)
+
+  $('#menu-wrapper__close-button').click(function() {
+    $('.menu-wrapper').removeClass('active');
+    $('#hamburger__main').removeClass('is-active');
+  })
+ 
   $('#s-main__btn').click(function() {
     vex.dialog.open({
       message: 'Обратная связь',
       input: [
-        ${''},
-        '<input name="email" type="email" placeholder="Как к Вам обращаться?" required />',
-        '<textarea placeholder="Password" required /></textarea>'
+        `<input name="email" type="email" placeholder="Как к Вам обращаться?" required />
+        <textarea class="s-main__textarea" placeholder="Сообщение" required /></textarea>`
       ].join(''),
       buttons: [
-        $.extend({}, vex.dialog.buttons.YES, { text: 'Login' }),
-        $.extend({}, vex.dialog.buttons.NO, { text: 'Back' })
+        $.extend({}, vex.dialog.buttons.YES, { text: 'Отправить' }),
+        // $.extend({}, vex.dialog.buttons.NO, { text: 'Назад' })
       ],
       callback: function (data) {
-        if (!data) {
-          console.log('Cancelled')
-        } else {
-          console.log('Username', data.username, 'Password', data.password)
-        }
+        // if (!data) {
+        //   console.log('Cancelled')
+        // } else {
+        //   console.log('Username', data.username, 'Password', data.password)
+        // }
       }
     })
   })
